7ca3561a10efee70e614f161350bd4e640cb5a33
sezfabian <cheruiyotfabian@gmail.com>
2023-08-03 08:31:38 +0300

    pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Module defines function for pascal Triangle
 """
 
+
 def pascal_triangle(n):
     """
     Return a list of lists of integers representing the Pascal's triangle
@@ -11,17 +12,18 @@ def pascal_triangle(n):
 
     if n <= 0:
         return Myarray
-    
+
     Myarray.append([1])
-    
+
     if n == 1:
         return Myarray
-    
+
     row = [1, 1]
     Myarray.append(row)
 
     for i in range(2, n):
-        row = [1] + [Myarray[i-1][j-1] + Myarray[i-1][j] for j in range(1, i)] + [1]
+        row = [1] + [Myarray[i-1][j-1]
+                     + Myarray[i-1][j] for j in range(1, i)] + [1]
         Myarray.append(row)
-        
+
     return Myarray
