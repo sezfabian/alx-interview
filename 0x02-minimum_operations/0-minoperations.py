ab16c1f7d01c936f0ef96828c456974cd7b1c042
sezfabian <cheruiyotfabian@gmail.com>
2023-08-11 03:26:31 +0300

    min nums

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,12 +9,15 @@ def minOperations(n: int) -> int:
     Return the  fewest number of operations needed
     to result in exactly n H characters in the file.
     """
-    if n <= 1:
+    if n == 1:
         return 0
-
-    operations: int = 0
-    for i in range(min_operations, n + 1):
-        while n % i == 0:
-            n = n / i
-            operations += i
-    return operations
+    
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + (i // j))
+    
+    return dp[n] if dp[n] != float('inf') else 0
