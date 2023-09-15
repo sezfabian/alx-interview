3b7dad930053c335bd7f3d5de0ef0a01285a23ce
sezfabian <cheruiyotfabian@gmail.com>
2023-09-15 03:02:23 +0300

    rotate ycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """0. Rotate 2D Matrix"""
 
+
 def rotate_2d_matrix(matrix):
     """Rotate 2D matrix"""
     for row in range(len(matrix)):
         for col in range(row, len(matrix)):
-            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+            matrix[row][col], matrix[col][row] =\
+                matrix[col][row], matrix[row][col]
 
     for row in matrix:
         row.reverse()
 
-    return matrix        
+    return matrix
