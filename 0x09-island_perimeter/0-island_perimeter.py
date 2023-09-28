1ec846d1eec020249de8bf5d325b00d9be5271e0
sezfabian <cheruiyotfabian@gmail.com>
2023-09-29 01:37:12 +0300

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,13 +11,14 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if grid[i-1][j] == 0:
-                    perimeter += 1
-                if grid[i+1][j] == 0:
-                    perimeter += 1
-                if grid[i][j-1] == 0:
-                    perimeter += 1
-                if grid[i][j+1] == 0:
-                    perimeter += 1
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i+1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j+1] == 1:
+                    perimeter -= 1
 
     return perimeter
